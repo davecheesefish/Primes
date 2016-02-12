060d36539b5988f6ed9ac752d53a8d3ad73625cd
David Prior <david.prior@davecheesefish.com>
2016-02-12 22:36:05 +0000

    Initial commit.

---
+++ Primes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            // Keep the console open to show results.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
