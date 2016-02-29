96952bfc99586bf52bdb4a1ab4ea9e507b7bc7cd
David Prior <david.prior@davecheesefish.com>
2016-02-29 13:57:16 +0000

    Added QUnit test setup.

--- javascript/build/build.js
+++ javascript/build/build.js
@@ -4,18 +4,37 @@
  */
 (function(){
 	'use strict';
-	console.log('Build started.');
 	
+	// File paths
+	var inCodePath     = 'src/primes.js',          // Source path
+		outCodePath    = 'dist/primes.js',         // Compiled output path
+		outMinCodePath = 'dist/primes.min.js',     // Minified output path
+		outMinMapPath  = 'dist/primes.min.js.map'; // Minified source map output path
 	
 	// Import required modules.
 	var fs       = require('fs'),
 		uglifyJs = require('uglify-js');
 	
-	var code;
+	// Declare other variables.
+	var code, uglified;
+	
+	
+	console.log('Build started.');
+	
+	// Compile code (a straight copy for now).
+	code = fs.readFileSync(inCodePath);
+	fs.writeFileSync(outCodePath, code);
 	
 	// Minify code.
-	code = uglifyJs.minify('src/primes.js', {warnings: true}).code;
-	fs.writeFileSync('dist/primes.min.js', code); // Write to file
+	uglified = uglifyJs.minify(
+		outCodePath,
+		{
+			warnings: true,
+			outSourceMap: outMinMapPath
+		}
+	);
+	fs.writeFileSync(outMinCodePath, uglified.code); // Write code to file
+	fs.writeFileSync(outMinMapPath, uglified.map); // Write map to file
 	
 	
 	console.log('Build complete.');
---
+++ javascript/test/tests/primes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function(){
+	QUnit.module('Main');
+	
+	// Insert tests here
+})();
