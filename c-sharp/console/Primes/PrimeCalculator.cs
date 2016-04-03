941f01aa6d830734b7bcc0844942d6cd1c5497c7
David Prior <david.prior@davecheesefish.com>
2016-04-03 22:33:59 +0100

    Moved C# implementation to console subdirectory in preparation for other implementations.

--- c-sharp/console/Primes/PrimeCalculator.cs
+++ c-sharp/console/Primes/PrimeCalculator.cs
@@ -16,33 +16,36 @@ namespace Primes
         /// </summary>
         /// <param name="count">The number of primes to calculate.</param>
         /// <returns>A list of the first (count) prime numbers.</returns>
-        public static List<int> GetFirstPrimes(int count){
+        public static List<int> GetFirstPrimes(int count)
+        {
             if (count < 0)
             {
                 count = 0;
             }
 
             List<int> primes = new List<int>();
-            
+
             // Since 2 is the only even prime number, we can shortcut adding it to the list to
             // avoid the overhead of having a special case in the main checking loop.
-            if (count > 0){
+            if (count > 0)
+            {
                 primes.Add(2);
             }
-        
+
             int primeCandidate = 3; // Current candidate for being prime.
             bool divisible = false;
             while (primes.Count < count) // While we have fewer primes than asked for...
             {
                 divisible = false;
-                foreach (int prime in primes){ // Check the candidate is not divisible by any lower primes.
+                foreach (int prime in primes) // Check the candidate is not divisible by any lower primes.
+                {
                     if (prime * prime > primeCandidate)
                     {
                         // We only need to check divisibility of primes up to the square root of
                         // the candidate, so break if prime^2 is more than it.
                         break;
                     }
-                    
+
                     if (primeCandidate % prime == 0)
                     {
                         // Mark as divisible if there is no remainder.
